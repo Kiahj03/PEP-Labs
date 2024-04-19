f0a0f50bd49e181228d149dbb16aa36e7ad31a56
Kiahj03 <90876523+Kiahj03@users.noreply.github.com>
2024-04-19 15:16:13 -0400

    Palindrome Lab

---
+++ StringIsPalindrome/src/main/java/Palindrome.java
@@ -0,0 +1,26 @@
+
+public class Palindrome {
+    /**
+     * Palindromes are words that are the same forwards as they are backwards - eg, 'bob', 'racecar'.
+     *
+     * You can solve this problem in several ways:
+     * You could start with a String reversal algorithm, which you have already written, and check if str is the same
+     * as its reversed form, or, you could check if the character at the nth position of a String moving forwards
+     * re the same as the nth position of a String moving backwards.
+     *
+     * @param str A String.
+     * @return true if str is a palindrome, false otherwise.
+     */
+    public boolean pal(String str){
+        String rev = "";
+
+        char[] arr = str.toCharArray();
+        for(int i = arr.length - 1; i >= 0; i--){
+           rev += arr[i]; 
+        }
+        if(rev.equals(str)){
+            return true;
+        }
+        return false;
+    }
+}
