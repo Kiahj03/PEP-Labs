4b1c445e2fe855cd05d34e2b7b5ebd2b7d541148
Kiahj03 <90876523+Kiahj03@users.noreply.github.com>
2024-04-19 20:57:28 -0400

    MaxMinusMin Lab

---
+++ MaxMinusMin/src/main/java/MaxMinusMin.java
@@ -0,0 +1,25 @@
+
+import java.util.List;
+
+public class MaxMinusMin {
+    /**
+     * Return the difference between the highest and lowest values of the List.
+     * This will require you to know some of the List interface methods.
+     *
+     * @param nums an List<Integer> containing varying nums.
+     * @return the max value of nums minus the min value of nums.
+     */
+    public int difference(List<Integer> nums){
+        int max = nums.get(0);
+        int min = nums.get(0);
+        for(int x = 0; x < nums.size(); x++){
+            if(nums.get(x) > max){
+                max = nums.get(x);
+            }
+            if(nums.get(x) < min){
+                min = nums.get(x);
+            }
+        }
+        return max - min;
+    }
+}
