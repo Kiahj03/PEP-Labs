6b69a66537a319207b88f62b17af550cd7bc3769
Kiahj03 <90876523+Kiahj03@users.noreply.github.com>
2024-04-19 20:49:49 -0400

    IsSorted Lab

---
+++ IsSorted/src/main/java/IsSorted.java
@@ -0,0 +1,17 @@
+public class IsSorted {
+    /**
+     * Return true if arr is sorted, smallest to largest, false otherwise.
+     * For example, {1,2,3,4} is storted but {1,2,4,3} is unsorted
+     *
+     * @param arr an int array
+     * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
+     */
+    public boolean checkSort(int[] arr){
+        for(int x = 0; x < arr.length-1; x++){
+            if(arr[x] > arr[x+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
