50016214725bb40a10b0cc89acfaeee4c2fdef39
Kiahj03 <90876523+Kiahj03@users.noreply.github.com>
2024-04-19 20:04:06 -0400

    IsThereSum Lab

---
+++ IsThereSum/src/main/java/IsThereSum.java
@@ -0,0 +1,22 @@
+
+public class IsThereSum {
+    /**
+     * Check if there exist two numbers in an array add up to some other number.
+     * You will likely need a for loop nested inside of another for loop for this problem - each for loop checking
+     * sums of two different indexes of arr.
+     *
+     * @param arr an array of ints.
+     * @param target a hypothetical sum of two numbers.
+     * @return true if two separate values in the array add up to a target, false otherwise.
+     */
+    public boolean check(int[] arr, int target){
+        for(int x = 0; x < arr.length; x++){
+            for(int y = x + 1; y < arr.length; y++){
+                if(arr[x] + arr[y] == target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
