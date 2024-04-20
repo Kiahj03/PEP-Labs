29d60bcadd203105b8e0d96e984f3fca0e35434c
Kiahj03 <90876523+Kiahj03@users.noreply.github.com>
2024-04-19 20:50:25 -0400

    IndexOf Lab

---
+++ IndexOf/src/main/java/IndexOf.java
@@ -0,0 +1,23 @@
+
+//
+
+public class IndexOf {
+    /**
+     * Find the position of a number in an array.
+     * For example, the position of 7 in {0,1,7,3,4} is 2.
+     *
+     * @param arr an int array.
+     * @param n a possible value of arr.
+     * @return the first index (position) of n in arr. if n does not exist in arr, return -1.
+     */
+    public int getIndex(int[] arr, int n){
+        int index = -1;
+        for(int x = 0; x < arr.length; x++){
+            if(arr[x] == n){
+                index = x;
+                break;
+            }
+        }
+        return index;
+    }
+}
